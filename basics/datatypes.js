1983441cb6d55cd66b53c23596ba496abed34031
KhanTaufeeq <88697922+KhanTaufeeq@users.noreply.github.com>
2023-05-06 18:33:54 +0000

    datatypes of js

---
+++ basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all JS code as newer version
+
+// Number
+// string
+// boolean
+// Null
+// Undefined
+// bigInt
+// symbol
+// object
+
+console.log(typeof null);      // object    --> this is confusing thing about JavaScript
+console.log(typeof undefined);  // undefined
+console.log(typeof 18);
+console.log(typeof 'taufeeq');
+console.log(typeof 18.96);
---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 'Taufeeq_Khan';
+
+// const accountId = 'Khan_Taufeeq'  // not allowed
+
+let accountNum = 4567867;
+
+// let accountNum = 4567567;     // allowed
+
+let accountPassword;  // allowed
+// const nothing;   not allowed
+
+// console.log(accountNum);
+
+console.table([accountId,accountNum, accountPassword]);
+
