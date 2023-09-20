d5c3eadbbbe6d2f89de4bd93f73269c8948f62a2
KhanTaufeeq <88697922+KhanTaufeeq@users.noreply.github.com>
2023-09-20 17:07:56 +0000

    datatype-summary

---
+++ basics/datatypes-summary.js
@@ -0,0 +1,22 @@
+// primitive data types --> call by value
+// 7 types --> string, Number, null, boolean, symbol, bigInt, undefined
+
+// reference data type
+// Arrays, Objects, Functions
+
+const name = 'Taufeeq';
+const age = 26;
+const loggedIn = false;
+const temp = null;
+let x;
+let id = Symbol('12');
+let anotherId = Symbol('12');
+
+const grocery = ['milk','bread','salt','butter'];
+
+const myFunc = function(){
+    console.log('Taufeeq')
+}
+
+console.log(id);
+console.log(anotherId);
